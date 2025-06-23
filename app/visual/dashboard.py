96cc8b4c4a79d482a6e5d6543e4f693a1848e3fb
root <root@vds2775649.my-ihor.ru>
2025-06-23 06:27:19 +0300

    версия с vds

--- app/visual/dashboard.py
+++ app/visual/dashboard.py
@@ -4,11 +4,15 @@ from dash_auth import BasicAuth
 from app.visual.components import create_layout
 from dash_auth import BasicAuth
 import secrets
+import yaml
+with open("app/config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+DASH_AUTH_CREDENTIALS = {
+    config["auth"]["username"]: config["auth"]["password"]
+}
+
 
 SECRET_KEY = secrets.token_hex(16)  # Генерируем случайный ключ
-DASH_AUTH_CREDENTIALS = {
-    "admin": "admin"  # логин: пароль
-}
 logger = setup_logger()
 dash_app = Dash(__name__, assets_folder="../static")
 dash_app.server.secret_key = SECRET_KEY  # Устанавливаем секретный ключ
